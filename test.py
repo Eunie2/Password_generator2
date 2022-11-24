db8d12fdde89e8db164414065614fa405a44ec69
eunie <ipad.yuya.514@gmail.com>
2022-11-24 16:38:52 +0900

    first commit

---
+++ test.py
@@ -0,0 +1,7 @@
+print("Who are you? ")
+user_name = input()
+if user_name == "eunie":
+    print("Hi " + user_name + "! Have a nice day!")
+else:
+    print("Hey! " + user_name + "! What's up?")
+    
